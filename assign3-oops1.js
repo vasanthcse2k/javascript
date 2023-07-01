cd6a406a6cf1bede029ffbeae9e8addc2efec510
Vasanth S <115918786+vasanthcse2k@users.noreply.github.com>
2023-07-01 15:30:18 +0530

    Add files via upload

---
+++ assign3-oops1.js
@@ -0,0 +1,303 @@
+const myPersonalDetails = [
+    {
+      "_id": "5f26e720db51caf71bf56131",
+      "index": 0,
+      "guid": "b957fa1e-56b2-472a-8397-0ef82e9ba144",
+      "isActive": false,
+      "balance": "$2,862.87",
+      "picture": "http://placehold.it/32x32",
+      "age": 35,
+      "eyeColor": "blue",
+      "name": "Teresa Greene",
+      "gender": "female",
+      "company": "COMVERGES",
+      "authors": [
+        {
+          "authorId": "teresagreene@comverges.com"
+        },
+        {
+          "authorId": "teresagreene@comverges.com"
+        },
+        {
+          "authorId": "teresagreene@comverges.com"
+        },
+        {
+          "authorId": "teresagreene@comverges.com"
+        },
+        {
+          "authorId": "teresagreene@comverges.com"
+        }
+      ],
+      "phone": "+1 (839) 578-2261",
+      "address": "226 Ridgecrest Terrace, Goochland, Washington, 3267",
+      "about": "Exercitation culpa nisi anim eiusmod ex ea elit incididunt id sunt ipsum ullamco cupidatat laborum. Commodo elit dolore proident aliquip deserunt ea excepteur. Est duis dolore exercitation labore eiusmod mollit do esse enim quis eu irure. Deserunt voluptate ad eu dolore. Ex laborum ad incididunt et nostrud voluptate nulla Lorem anim ullamco minim cupidatat. Voluptate commodo dolore excepteur et reprehenderit nostrud ea nostrud eiusmod commodo mollit ea. Quis deserunt culpa nisi ut cupidatat.\r\n",
+      "registered": "2014-09-21T03:22:28 -06:-30",
+      "latitude": -83.395928,
+      "longitude": 170.40198,
+      "tags": [
+        "enim",
+        "ipsum",
+        "adipisicing",
+        "aute",
+        "dolor",
+        "eiusmod",
+        "id"
+      ],
+      "friends": [
+        {
+          "name": "Dollie Jefferson"
+        },
+        {
+          "name": "Nita Le"
+        },
+        {
+          "name": "Davenport Obrien"
+        }
+      ],
+      "greeting": "Hello, Teresa Greene! You have 5 unread messages.",
+      "favoriteFruit": "banana"
+    },
+    {
+      "_id": "5f26e72044f422b0080261ba",
+      "index": 1,
+      "guid": "266b1481-4942-4503-ab89-d53bdfc73635",
+      "isActive": true,
+      "balance": "$1,563.58",
+      "picture": "http://placehold.it/32x32",
+      "age": 33,
+      "eyeColor": "brown",
+      "name": "Mcdowell Maynard",
+      "gender": "male",
+      "company": "RETRACK",
+      "authors": [
+        {
+          "authorId": "mcdowellmaynard@retrack.com"
+        },
+        {
+          "authorId": "mcdowellmaynard@retrack.com"
+        },
+        {
+          "authorId": "mcdowellmaynard@retrack.com"
+        },
+        {
+          "authorId": "mcdowellmaynard@retrack.com"
+        },
+        {
+          "authorId": "mcdowellmaynard@retrack.com"
+        }
+      ],
+      "phone": "+1 (908) 468-2941",
+      "address": "950 Richardson Street, Barronett, New York, 8082",
+      "about": "Elit sint duis fugiat occaecat adipisicing elit. Ea Lorem occaecat minim est labore do elit officia ea labore in amet sunt consectetur. Minim cillum consectetur elit aliqua dolore. Aute aute ut aliquip et ullamco fugiat ut veniam tempor occaecat tempor irure veniam.\r\n",
+      "registered": "2017-06-12T10:36:13 -06:-30",
+      "latitude": -16.65641,
+      "longitude": -77.90467,
+      "tags": [
+        "minim",
+        "et",
+        "in",
+        "aute",
+        "elit",
+        "pariatur",
+        "cupidatat"
+      ],
+      "friends": [
+        {
+          "name": "Anita Wade"
+        },
+        {
+          "name": "York Gomez"
+        },
+        {
+          "name": "Nancy Riddle"
+        }
+      ],
+      "greeting": "Hello, Mcdowell Maynard! You have 1 unread messages.",
+      "favoriteFruit": "apple"
+    },
+    {
+      "_id": "5f26e7205375ac58b9b159dd",
+      "index": 2,
+      "guid": "0707c048-f774-4c8a-ac58-ad96310c48f2",
+      "isActive": false,
+      "balance": "$2,966.07",
+      "picture": "http://placehold.it/32x32",
+      "age": 31,
+      "eyeColor": "brown",
+      "name": "Tanisha Mullen",
+      "gender": "female",
+      "company": "RECRISYS",
+      "authors": [
+        {
+          "authorId": "tanishamullen@recrisys.com"
+        },
+        {
+          "authorId": "tanishamullen@recrisys.com"
+        },
+        {
+          "authorId": "tanishamullen@recrisys.com"
+        },
+        {
+          "authorId": "tanishamullen@recrisys.com"
+        },
+        {
+          "authorId": "tanishamullen@recrisys.com"
+        }
+      ],
+      "phone": "+1 (974) 499-3810",
+      "address": "236 Berkeley Place, Westerville, Nevada, 4617",
+      "about": "Pariatur laborum ea enim est consectetur ipsum. Occaecat ullamco ea ad nostrud labore id nisi. Cupidatat Lorem qui commodo ex laboris proident tempor.\r\n",
+      "registered": "2019-03-31T05:50:24 -06:-30",
+      "latitude": -37.761847,
+      "longitude": -45.282861,
+      "tags": [
+        "eiusmod",
+        "minim",
+        "aliqua",
+        "nisi",
+        "ex",
+        "ipsum",
+        "do"
+      ],
+      "friends": [
+        {
+          "name": "Dodson Heath"
+        },
+        {
+          "name": "Harrington Cote"
+        },
+        {
+          "name": "Aisha Gonzalez"
+        }
+      ],
+      "greeting": "Hello, Tanisha Mullen! You have 10 unread messages.",
+      "favoriteFruit": "strawberry"
+    },
+    {
+      "_id": "5f26e720815c76852c06f235",
+      "index": 3,
+      "guid": "65b2ad9b-5218-43ab-a39a-2329b27f5108",
+      "isActive": true,
+      "balance": "$3,095.99",
+      "picture": "http://placehold.it/32x32",
+      "age": 20,
+      "eyeColor": "blue",
+      "name": "Pitts Moran",
+      "gender": "male",
+      "company": "CYCLONICA",
+      "authors": [
+        {
+          "authorId": "pittsmoran@cyclonica.com"
+        },
+        {
+          "authorId": "pittsmoran@cyclonica.com"
+        },
+        {
+          "authorId": "pittsmoran@cyclonica.com"
+        },
+        {
+          "authorId": "pittsmoran@cyclonica.com"
+        },
+        {
+          "authorId": "pittsmoran@cyclonica.com"
+        }
+      ],
+      "phone": "+1 (856) 445-2491",
+      "address": "843 Concord Street, Bentley, Federated States Of Micronesia, 9208",
+      "about": "Qui cillum nisi aute elit. Ad Lorem in sit veniam magna do qui aliquip fugiat officia consequat. Amet tempor do veniam pariatur ipsum velit et anim velit fugiat aute. Ex commodo laboris eu non adipisicing et aute consequat aliqua officia commodo fugiat anim amet. Fugiat irure deserunt deserunt ad laboris occaecat. Est nostrud voluptate in deserunt minim minim ad id.\r\n",
+      "registered": "2016-04-03T02:52:27 -06:-30",
+      "latitude": 89.864277,
+      "longitude": 9.443314,
+      "tags": [
+        "exercitation",
+        "cillum",
+        "officia",
+        "mollit",
+        "enim",
+        "occaecat",
+        "est"
+      ],
+      "friends": [
+        {
+          "name": "Cunningham Ryan"
+        },
+        {
+          "name": "Campbell Morales"
+        },
+        {
+          "name": "Rita Wallace"
+        }
+      ],
+      "greeting": "Hello, Pitts Moran! You have 5 unread messages.",
+      "favoriteFruit": "strawberry"
+    },
+    {
+      "_id": "5f26e720d4b8f2a8d543daef",
+      "index": 4,
+      "guid": "d0bcb507-6536-42ef-8498-478c063fe612",
+      "isActive": false,
+      "balance": "$2,822.83",
+      "picture": "http://placehold.it/32x32",
+      "age": 26,
+      "eyeColor": "green",
+      "name": "Laurie Knowles",
+      "gender": "female",
+      "company": "PETIGEMS",
+      "authors": [
+        {
+          "authorId": "laurieknowles@petigems.com"
+        },
+        {
+          "authorId": "laurieknowles@petigems.com"
+        },
+        {
+          "authorId": "laurieknowles@petigems.com"
+        },
+        {
+          "authorId": "laurieknowles@petigems.com"
+        },
+        {
+          "authorId": "laurieknowles@petigems.com"
+        }
+      ],
+      "phone": "+1 (940) 508-2070",
+      "address": "897 Duryea Place, Waiohinu, District Of Columbia, 3641",
+      "about": "Ipsum nisi et dolor elit labore exercitation amet nulla id ipsum eu cillum velit eu. Excepteur exercitation esse ullamco ipsum duis non anim. Cillum amet anim deserunt reprehenderit sint aliqua aliqua veniam. Sit est ex cillum mollit amet est mollit mollit voluptate.\r\n",
+      "registered": "2015-03-25T12:57:46 -06:-30",
+      "latitude": 22.303218,
+      "longitude": 119.041903,
+      "tags": [
+        "aliquip",
+        "veniam",
+        "officia",
+        "enim",
+        "anim",
+        "minim",
+        "anim"
+      ],
+      "friends": [
+        {
+          "name": "Lucy Reeves"
+        },
+        {
+          "name": "Banks Floyd"
+        },
+        {
+          "name": "Long Lewis"
+        }
+      ],
+      "greeting": "Hello, Laurie Knowles! You have 2 unread messages.",
+      "favoriteFruit": "strawberry"
+    }
+  ]
+    function removeDuplicates(array) {
+    return [new Set(array)];
+  }
+    for (const key in myPersonalDetails) {
+    if (Array.isArray(myPersonalDetails[key])) {
+      myPersonalDetails[key] = removeDuplicates(myPersonalDetails[key]);
+    }
+  }
+  const result = Object.values(myPersonalDetails).flat();
+  console.log(result);
+  
